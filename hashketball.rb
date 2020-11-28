cbeb0c1066ac601f39ae4863050c07df89eba462
Esma Sherwani <esmasherr@gmail.com>
2020-11-28 21:46:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,41 @@
 require 'pry'
+
 def game_hash
   { 
     :home { 
       teamname: "Brooklyn Nets",
     colors : ["Black", "White"],
-    players: 
+    players: {
+      "Alan Anderson" => {
+        number: 0,
+        shoe: 16,
+        points: 22,
+        rebounds: 12,
+        assists: 12,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 1
+
+      },
+      "Reggie Evans" => {
+        numer: 30,
+        shoe: 14,
+        points: 12,
+        rebounds: 12,
+        assists: 12,
+        steals: 12,
+        blocks: 12,
+        slam_dunks: 7
+      },
+      "Brook Lopez" => {
+        numer: 11,
+        shoe: 17,
+        points: 17,
+        rebounds: 19,
+        assists: 10,
+        steals: 3,
+        blocks: 12,
+        slam_dunks: 7
+      }
+    } 
   
